5d23da795bab1bf018e76904df113c6bdfb882b8
Alec Blakeley <alecblkly@gmail.com>
2020-02-21 13:56:19 -0500

    No occurences/occurences are printing for instances of th. If statement for recursion is incorrect, not allowing for occurences to be added to return proper count.

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,8 +3,16 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
+
+
 def count_th(word):
-    
-    # TBC
-    
-    pass
+    th_words = word.find("th")
+
+    if th_words == 0:
+        print("No occurences found.")
+        return 0
+    if th_words >= 1:
+        print("Occurences were found")
+        return 1 + count_th(word[1:])
+    else:
+        return count_th(word[1:])
