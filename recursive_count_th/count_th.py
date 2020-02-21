00fbe4846fdc372a171ba60f1b4c56e32b42354a
Alec Blakeley <alecblkly@gmail.com>
2020-02-21 18:06:57 -0500

    Count_th is passing all tests (MVP Complete).

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -6,13 +6,13 @@ Your function must utilize recursion. It cannot contain any loops.
 
 
 def count_th(word):
-    th_words = word.find("th")
-
-    if th_words == 0:
-        print("No occurences found.")
+    # Checking to see if the length of the word is less than 2, as "th" is two letters
+    if len(word) < 2:
         return 0
-    if th_words >= 1:
-        print("Occurences were found")
+    if word[0:2] == "th":
+        # Moving through the string 2 letters ("th") at a time
+        # If two letters side-by-side are "th", then the value increments by 1
+        # This continues until hitting the end of the string
         return 1 + count_th(word[1:])
     else:
         return count_th(word[1:])
